b0a65b4c3b1ed4907aebfd41e7a8f4867c79d4aa
Sardor Toirov <toirovsardor62@gmail.com>
2025-03-17 16:51:14 +0500

    admin panel

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t#!33&9q2%3ssxnyk_g-cn(tw7#($il_tc(kz*x=rivfe7=bph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['admin-panel-university-2.onrender.com','localhost']
+ALLOWED_HOSTS = ['*','admin-panel-university-2.onrender.com','localhost']
 
 
 # Application definition
