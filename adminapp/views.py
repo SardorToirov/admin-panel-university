98fc782259dbf95cb3b3a7e808a94478d5a1f052
Sardor Toirov <toirovsardor62@gmail.com>
2025-03-17 17:18:19 +0500

    register page

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 from .forms import *
 from . import services
 
@@ -373,3 +377,8 @@ def student_list(request):
 @login_required_decorator
 def profile(request):
     return render(request,'profile.html')
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login_page")
+    template_name = "signup.html"
